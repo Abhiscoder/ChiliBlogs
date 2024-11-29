5d3fb6eb1217570ae3ae86a56c80ad3d5feadc06
Abhishek Kumar <115029976+Abhiscoder@users.noreply.github.com>
2024-11-29 17:43:23 +0530

    Update App.jsx
    extencion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./comonents/Footer.jsx";
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import Blogs from "../src/pages/Blogs.jsx";
 import About from "../src/pages/About.jsx";
-import Login from "../src/pages/Login..jsx";
+import Login from "../src/pages/Login.jsx";
 import Register from "../src/pages/Register.jsx";
 import Dashboard from "../src/pages/Dashboard.jsx";
 import Contact from "../src/pages/Contact.jsx";
