b29235adf0c79445b20a68ae772bd02f94747435
Abhishek Kumar <115029976+Abhiscoder@users.noreply.github.com>
2024-11-29 17:41:56 +0530

    Update App.jsx
    add the extension  .jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import Navbar from './comonents/Navbar';
-import Home from "./comonents/Home";
-import Footer from "./comonents/Footer";
+import Navbar from './comonents/Navbar.jsx';
+import Home from "./comonents/Home.jsx";
+import Footer from "./comonents/Footer.jsx";
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
-import Blogs from "../src/pages/Blogs";
-import About from "../src/pages/About";
-import Login from "../src/pages/Login";
-import Register from "../src/pages/Register";
-import Dashboard from "../src/pages/Dashboard";
-import Contact from "../src/pages/Contact";
-import Creators from "../src/pages/Creators";
+import Blogs from "../src/pages/Blogs.jsx";
+import About from "../src/pages/About.jsx";
+import Login from "../src/pages/Login..jsx";
+import Register from "../src/pages/Register.jsx";
+import Dashboard from "../src/pages/Dashboard.jsx";
+import Contact from "../src/pages/Contact.jsx";
+import Creators from "../src/pages/Creators.jsx";
 import { useAuth } from './context/AuthProvider';
 import { Toaster } from 'react-hot-toast';
-import UpdateBlog from './dashboard/UpdateBlog';
-import Detail from './pages/Detail';
+import UpdateBlog from './dashboard/UpdateBlog.jsx';
+import Detail from './pages/Detail.jsx';
 // import NotFound from './pages/Notfound';
 
 function App() {
